bb84fe0bea54e5784f7ac0f83f2abc939c0366d5
intx <francesco.intoci@epfl.ch>
2021-04-21 15:18:10 +0200

    Working on balanced

--- src/main/scala/lsh/BaseConstruction.scala
+++ src/main/scala/lsh/BaseConstruction.scala
@@ -6,8 +6,7 @@ import lsh.MinHash
 class BaseConstruction(sqlContext: SQLContext, data: RDD[(String, List[String])], seed : Int) extends Construction {
   //build buckets here
   val minHash = new MinHash(seed)
-  val hashData: RDD[(String, Int)] = minHash.execute(data)
-  val buckets: RDD[(Int, Set[String])] = hashData.groupBy(f => f._2)
+  val buckets: RDD[(Int, Set[String])] = minHash.execute(data).groupBy(f => f._2)
     .map(f => (f._1, f._2.map(t => t._1).toSet))
 
   override def eval(queries: RDD[(String, List[String])]): RDD[(String, Set[String])] = {
@@ -15,7 +14,9 @@ class BaseConstruction(sqlContext: SQLContext, data: RDD[(String, List[String])]
     // First compute MinHash on the queries and map it to (Int, String)
     // Then compute the join with the buckets and map it to a form String, Option[Set[String]]
     // Filter out those Options which are empty and map it to String, Set[String]
+
     val hashQueries: RDD[(Int, String)] = minHash.execute(queries).map(f => (f._2, f._1))
+
     hashQueries.leftOuterJoin(buckets).map(f => (f._2._1, f._2._2))
       .filter(f => f._2.nonEmpty).map(f => (f._1, f._2.get))
   }
--- src/main/scala/lsh/BaseConstructionBalanced.scala
+++ src/main/scala/lsh/BaseConstructionBalanced.scala
@@ -5,21 +5,50 @@ import org.apache.spark.sql.SQLContext
 
 class BaseConstructionBalanced(sqlContext: SQLContext, data: RDD[(String, List[String])], seed : Int, partitions : Int) extends Construction {
   //build buckets here
+  val minHash = new MinHash(seed)
+  val buckets: RDD[(Int, Set[String])] = minHash.execute(data).groupBy(f => f._2)
+    .map(f => (f._1, f._2.map(t => t._1).toSet))
 
   def computeMinHashHistogram(queries : RDD[(String, Int)]) : Array[(Int, Int)] = {
-    //compute histogram for target buckets of queries
-
-    null
+    // compute histogram for target buckets of queries
+    // return histogram sorted by hashmin key ASC
+    queries.groupBy(f => f._2).map(f => (f._1, f._2.toArray.length))
+      .sortBy(f => f._1, ascending = true).collect()
+  }
+  def computeDepth(histogram: Array[(Int,Int)]) : Int = {
+    var d = 0
+    histogram.foreach(f => d = d + f._2)
+    (d/partitions).ceil.toInt
   }
 
   def computePartitions(histogram : Array[(Int, Int)]) : Array[Int] = {
     //compute the boundaries of bucket partitions
-
-    null
+    //partitions are computed returning the last element of the bucket
+    // i.e: histogram = [(id1,1),(id2,3),(id3,1)]
+    // partitions = 2
+    // depth = ceil(5/2)=3
+    // bounds = [id2] -> meaning (id1,id2), (id3, +inf) = (id3)
+    var bounds = Array[Int]()
+    var buffer = 0
+    val depth = computeDepth(histogram)
+    val iter = histogram.iterator
+    while (iter.hasNext){
+      val h = iter.next()
+      buffer += h._2
+      if (buffer > depth) {
+        bounds :+= h._1
+        buffer = 0
+      }
+    }
+    bounds
   }
 
   override def eval(queries: RDD[(String, List[String])]): RDD[(String, Set[String])] = {
     //compute near neighbors with load balancing here
+    val hashQueries = minHash.execute(queries)
+    val histogram = computeMinHashHistogram(hashQueries)
+    val bounds = computePartitions(histogram)
+
 
     null
   }
