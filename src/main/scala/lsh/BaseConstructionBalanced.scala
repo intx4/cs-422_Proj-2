4a193f3359eb73012711121fcfe0db959947bcfe
intx <francesco.intoci@epfl.ch>
2021-04-24 16:32:41 +0200

    bug fix in broadcast

--- src/main/scala/lsh/BaseConstructionBalanced.scala
+++ src/main/scala/lsh/BaseConstructionBalanced.scala
@@ -9,19 +9,20 @@ class BaseConstructionBalanced(sqlContext: SQLContext, data: RDD[(String, List[S
   val buckets: RDD[(Int, Set[String])] = minHash.execute(data).groupBy(f => f._2)
     .map(f => (f._1, f._2.map(t => t._1).toSet)).sortBy(f => f._1, ascending = true)
 
-  def computeMinHashHistogram(queries : RDD[(String, Int)]) : Array[(Int, Int)] = {
+  def computeMinHashHistogram(queries: RDD[(String, Int)]): Array[(Int, Int)] = {
     // compute histogram for target buckets of queries
     // return histogram sorted by hashmin key ASC as <id, number>
     queries.groupBy(f => f._2).map(f => (f._1, f._2.toArray.length))
       .sortBy(f => f._1, ascending = true).collect()
   }
-  def computeDepth(histogram: Array[(Int,Int)]) : Int = {
+
+  def computeDepth(histogram: Array[(Int, Int)]): Int = {
     var d = 0
     histogram.foreach(f => d = d + f._2)
-    (d/partitions).ceil.toInt
+    (d / partitions).ceil.toInt
   }
 
-  def computePartitions(histogram : Array[(Int, Int)]) : Array[Int] = {
+  def computePartitions(histogram: Array[(Int, Int)]): Array[Int] = {
     //compute the boundaries of bucket partitions
     //partitions are computed returning the last element of the partition
     // i.e: histogram = [(id1,1),(id2,3),(id3,1)]
@@ -32,7 +33,7 @@ class BaseConstructionBalanced(sqlContext: SQLContext, data: RDD[(String, List[S
     var buffer = 0
     val depth = computeDepth(histogram)
 
-    for (h <- histogram){
+    for (h <- histogram) {
       buffer += h._2
       if (buffer > depth) {
         bounds :+= h._1
@@ -40,35 +41,42 @@ class BaseConstructionBalanced(sqlContext: SQLContext, data: RDD[(String, List[S
       }
     }
     //see if one needs to add the last id of the histogram
-    if (histogram.last._1 != bounds.last){
-      bounds :+= histogram.last._1
+    if (histogram.nonEmpty && bounds.nonEmpty) {
+      if (histogram.last._1 != bounds.last) {
+        bounds :+= histogram.last._1
+      }
+      bounds
     }
     bounds
   }
-  def assignPartition(bounds: Array[Int], id: Int):Int = {
-    //ex: bounds = [2, 4, 5] meaning partitions are [0;2], (2;4], (4;5]
-    // id = 3
-    var assigned = 0
-    for (b <- bounds){
-      if (id > b){
-        assigned = assigned + 1
+
+  object MyFunctions {
+    def assignPartition(bounds: Array[Int], id: Int): Int = {
+      //ex: bounds = [2, 4, 5] meaning partitions are [0;2], (2;4], (4;5]
+      // id = 3
+      var assigned = 0
+      for (b <- bounds) {
+        if (id > b) {
+          assigned = assigned + 1
+        }
+        else {
+          return assigned
+        }
       }
-      else {
-        return assigned
-      }
+      assigned
     }
-    assigned
+
+    def partionJoin(queries: List[(String, Int)], buckets: List[(Int, List[String])]): List[(String, Set[String])] = {
+      //for each query in partition, extract the bucket from the partition and return the films in there
+      var joinRes = List[(String, Set[String])]()
+      for (query <- queries) {
+        val neighs = buckets.filter(p => p._1 == query._2).flatMap(f => f._2).toSet
+        joinRes = joinRes :+ (query._1, neighs)
+      }
+      joinRes
+    }
   }
 
-  def partionJoin( queries: List[(String, Int)], buckets: List[(Int, List[String])]): List[(String, Set[String])] = {
-    //for each query in partition, extract the bucket from the partition and return the films in there
-    var joinRes = List[(String, Set[String])]()
-    for (query <- queries){
-      val neighs = buckets.filter(p => p._1 == query._2).flatMap(f => f._2).toSet
-      joinRes = joinRes :+ (query._1, neighs)
-    }
-    joinRes
-  }
   override def eval(queries: RDD[(String, List[String])]): RDD[(String, Set[String])] = {
     //compute near neighbors with load balancing here
     //note that both buckets and queries are sorted by minHash ASC
@@ -78,17 +86,18 @@ class BaseConstructionBalanced(sqlContext: SQLContext, data: RDD[(String, List[S
 
     // each entry of this RDD is a List of queries belonging to the same partition
     val partitionedQueries: RDD[(Int, List[(String, Int)])] = hashQueries.sortBy(f => f._2)
-      .groupBy(f => assignPartition(bounds, f._2)).map(f => (f._1, f._2.toList))
+      .groupBy(f => MyFunctions.assignPartition(bounds, f._2))
+      .map(f => (f._1, f._2.toList))
     // each entry of this RDD is a List of <bucketId, List of films in bucket> belonging to same partition
     val partitionedBuckets: RDD[(Int, List[(Int, List[String])])] = buckets.sortBy(f => f._1)
-      .groupBy(f => assignPartition(bounds, f._1))
+      .groupBy(f => MyFunctions.assignPartition(bounds, f._1))
       .map(f => (f._1, f._2.map(t => (t._1,t._2.toList)).toList))
 
     // each entry is a List of queries<Film, hash> followed by a bucket <hash, List[films] >
     val joinedRdd = partitionedQueries.join(partitionedBuckets).map(f => (f._2._1, f._2._2))
 
     //see partionJoin
-    val result = joinedRdd.flatMap(f => partionJoin(f._1, f._2))
+    val result = joinedRdd.flatMap(f => MyFunctions.partionJoin(f._1, f._2))
 
     result
   }
--- src/main/scala/lsh/BaseConstructionBroadcast.scala
+++ src/main/scala/lsh/BaseConstructionBroadcast.scala
@@ -4,6 +4,8 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkContext
 import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.sql.functions.udf
+
 class BaseConstructionBroadcast(sqlContext: SQLContext, data: RDD[(String, List[String])], seed : Int) extends Construction with Serializable {
   //build buckets here
   val minHash = new MinHash(seed)
@@ -11,22 +13,26 @@ class BaseConstructionBroadcast(sqlContext: SQLContext, data: RDD[(String, List[
     .map(f => (f._1, f._2.map(t => t._1).toSet)).sortBy(f => f._1, ascending = true)
     .collect()
   // at this point, buckets should be effectively distributed in all workers
-  val sc: SparkContext = SparkContext.getOrCreate()
-  val broadcastBuckets: Broadcast[Array[(Int, Set[String])]] = sc.broadcast(buckets)
+  val broadcastBuckets: Broadcast[Array[(Int, Set[String])]] =
+    sqlContext.sparkSession.sparkContext.broadcast(buckets)
 
-  def getNeighs(id: Int, buckets: Array[(Int, Set[String])]) : Option[Set[String]] = {
-    for (b <- buckets) {
-      if (b._1 == id) {
-        return Option(b._2)
+  //define a wrapper object to make func serializable by spark
+  object MyFunctions {
+    def getNeighs(id: Int, buckets: Array[(Int, Set[String])]): Option[Set[String]] = {
+      for (b <- buckets) {
+        if (b._1 == id) {
+          return Option(b._2)
+        }
       }
+      None
     }
-    None
   }
+
   override def eval(queries: RDD[(String, List[String])]): RDD[(String, Set[String])] = {
 
     val hashQueries: RDD[(Int, String)] = minHash.execute(queries).map(f => (f._2, f._1))
 
-    hashQueries.map(f => (f._2, getNeighs(f._1, broadcastBuckets.value)))
+    hashQueries.map(f => (f._2, MyFunctions.getNeighs(f._1, broadcastBuckets.value)))
       .filter(f => f._2.nonEmpty).map(f => (f._1, f._2.get))
   }
 }
