7b31447b34078797b0995927849f5a96243e86e3
intx <francesco.intoci@epfl.ch>
2021-04-26 09:44:15 +0200

    added AND & OR constr

--- src/main/scala/lsh/ANDConstruction.scala
+++ src/main/scala/lsh/ANDConstruction.scala
@@ -1,10 +1,25 @@
 package lsh
 import org.apache.spark.rdd.RDD
 
+import scala.collection.mutable
+
 class ANDConstruction(children: List[Construction]) extends Construction {
   override def eval(rdd: RDD[(String, List[String])]): RDD[(String, Set[String])] = {
     //compute AND construction results here
+    // rdd is queries
+    val queries = rdd
+    //this queue should contain the results for each construction
+    var intermediateResults = mutable.Queue[RDD[(String, Set[String])]]()
 
-    null
+    for (child <- children){
+      intermediateResults += child.eval(queries)
+    }
+
+    while (intermediateResults.length >= 2){
+      val res1 = intermediateResults.dequeue()
+      val res2 = intermediateResults.dequeue()
+      intermediateResults += res1.join(res2).map(f =>(f._1, f._2._1.intersect(f._2._2)))
+    }
+    intermediateResults.dequeue()
   }
 }
--- src/main/scala/lsh/ORConstruction.scala
+++ src/main/scala/lsh/ORConstruction.scala
@@ -1,11 +1,23 @@
 package lsh
 
 import org.apache.spark.rdd.RDD
+import scala.collection.mutable
 
 class ORConstruction(children: List[Construction]) extends Construction {
   override def eval(rdd: RDD[(String, List[String])]): RDD[(String, Set[String])] = {
-    //compute OR construction results here
+    val queries = rdd
+    //this queue should contain the results for each construction
+    var intermediateResults = mutable.Queue[RDD[(String, Set[String])]]()
 
-    null
+    for (child <- children){
+      intermediateResults += child.eval(queries)
+    }
+
+    while (intermediateResults.length >= 2){
+      val res1 = intermediateResults.dequeue()
+      val res2 = intermediateResults.dequeue()
+      intermediateResults += res1.join(res2).map(f =>(f._1, f._2._1.union(f._2._2)))
+    }
+    intermediateResults.dequeue()
   }
 }
